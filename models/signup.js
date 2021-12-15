a790d384929524aeb916517ecf862c33e6cae3df
Vishnu MD <vishnudivakaran1@gmail.com>
2021-12-15 23:58:18 +0530

    Sign Up API feature added

---
+++ models/signup.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    userName: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const UserSchema = mongoose.model("userSchema", userSchema);
+
+module.exports = UserSchema;
